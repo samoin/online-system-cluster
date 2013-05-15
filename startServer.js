14a0bf7ff2850f89e4b53785711a32d2f0fcc97d
samoin <gigi_ly180@hotmail.com>
2013-05-15 14:47:42 +0800

    init rep
    init rep

---
+++ startServer.js
@@ -0,0 +1,9 @@
+/**
+ * User: samoin
+ * Date: 13-5-14
+ * Time: 上午10:55
+ * desc: start server with configuration
+ */
+var server = require("./server");
+var onlineServers = server.servers;
+onlineServers.createServer({logLv:0,port:443,slaveCount:2, 'transports': ["websocket", 'flashsocket' , "xhr-polling" , "jsonp-polling"],syncSec : 10});
